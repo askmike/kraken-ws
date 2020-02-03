164a85d266721e9f5489e12d5b249295478e65cc
TrasherDK <trasherdk@fumlersoft.dk>
2020-02-03 15:42:44 +0700

    Update deps and make output readable. (#1)
    * Update deps & add repository
    
    * Make output readable by JSON.stringify()

--- example.js
+++ example.js
@@ -7,9 +7,9 @@ const go = async() => {
   console.log(new Date, 'connecting...');
   await con.connect();
   console.log(new Date, 'connected');
-  const channelId = await con.subscribe('XBT/USD', 'book', {depth: 100});
+  const channelId = await con.subscribe('XMR/EUR', 'book', {depth: 10});
   console.log(new Date, 'subscribed');
-  con.on('channel:' + channelId, console.log);
+  con.on('channel:' + channelId, (data) => console.log(JSON.stringify(data)));
 }
 
 go();
