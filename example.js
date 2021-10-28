b2f50fd9f133d5ed87a0f3e138de7311b0f22cba
TrasherDK <trasherdk@fumlersoft.dk>
2021-10-28 05:20:24 +0200

    cleaning: format code according to npm standard

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
-import Connection from './index.js';
+import Connection from './index.js'
 
-const con = new Connection();
-let counter = 0;
+const con = new Connection()
 
 const getBookHeader = (data) => {
   console.log('[header] ', JSON.stringify(data), '\n\n\n')
@@ -12,12 +11,11 @@ const getBookUpdates = (data) => {
 }
 
 const go = async () => {
-
-  console.log(new Date, 'connecting...');
-  await con.connect();
-  console.log(new Date, 'connected');
-  const channelId = await con.subscribe('XMR/EUR', 'book', { depth: 10 });
-  console.log(new Date, `subscribed: ${channelId}`);
+  console.log(new Date(), 'connecting...')
+  await con.connect()
+  console.log(new Date(), 'connected')
+  const channelId = await con.subscribe('XMR/EUR', 'book', { depth: 10 })
+  console.log(new Date(), `subscribed: ${channelId}`)
 
   con.once('channel:' + channelId, (data) => {
     getBookHeader(data)
@@ -25,7 +23,6 @@ const go = async () => {
       getBookUpdates(data)
     })
   })
-
 }
 
-go();
+go()
--- index.js
+++ index.js
@@ -1,117 +1,113 @@
-import WebSocket from 'ws';
-import crypto from 'crypto';
-import EventEmitter from 'events';
+import WebSocket from 'ws'
+import crypto from 'crypto'
+import EventEmitter from 'events'
+// import { resolve } from 'path';
 
 class Connection extends EventEmitter {
+  constructor (url = 'wss://ws.kraken.com') {
+    super()
 
-  constructor(url = 'wss://ws.kraken.com') {
-    super();
+    this.url = url
+    this.connected = false
 
-    this.url = url;
-    this.connected = false;
+    this.pairs = {}
 
-    this.pairs = {};
-
-    this.lastMessageAt = 0;
+    this.lastMessageAt = 0
   }
 
-  disconnect() {
-    this.ws.disconnect();
+  disconnect () {
+    this.ws.disconnect()
   }
 
-  connect() {
-
-    if(this.connected) {
-      return;
+  connect () {
+    if (this.connected) {
+      return
     }
 
-    let readyHook;
-    this.onOpen = new Promise(r => readyHook = r);
+    let readyHook
+    this.onOpen = new Promise(resolve => { readyHook = resolve })
 
-    this.ws = new WebSocket(this.url);
+    this.ws = new WebSocket(this.url)
 
     this.ws.onopen = () => {
-      this.connected = true;
-      readyHook();
+      this.connected = true
+      readyHook()
     }
 
     this.ws.onerror = e => {
-      console.log(new Date, '[KRAKEN] error', e);
+      console.log(new Date(), '[KRAKEN] error', e)
     }
+
     this.ws.onclose = e => {
-      console.log(new Date, '[KRAKEN] close', e);
+      console.log(new Date(), '[KRAKEN] close', e)
     }
 
     // initial book data coming in on the same tick as the subscription data
     // we defer this so the subscription promise resloves before we send
     // initial OB data.
-    this.ws.onmessage = e => setImmediate(() => this.handleMessage(e));
+    this.ws.onmessage = e => setImmediate(() => this.handleMessage(e))
 
-    return this.onOpen;
+    return this.onOpen
   }
 
-  handleMessage = e => {
-    this.lastMessageAt = +new Date;
+  handleMessage (e) {
+    this.lastMessageAt = +new Date()
 
-    const payload = JSON.parse(e.data);
+    const payload = JSON.parse(e.data)
 
-    if(Array.isArray(payload)) {
-      this.emit('channel:' + payload[0], payload);
+    if (Array.isArray(payload)) {
+      this.emit('channel:' + payload[0], payload)
     } else {
-
-      if(payload.event === 'subscriptionStatus' && payload.status === 'subscribed') {
-
-        if(this.pairs[payload.pair]) {
-          this.pairs[payload.pair].id = payload.channelID;
-          this.pairs[payload.pair].onReady(payload.channelID);
+      if (payload.event === 'subscriptionStatus' && payload.status === 'subscribed') {
+        if (this.pairs[payload.pair]) {
+          this.pairs[payload.pair].id = payload.channelID
+          this.pairs[payload.pair].onReady(payload.channelID)
         } else {
-          console.log(new Date, '[KRAKEN] received subscription event for unknown subscription', payload);
+          console.log(new Date(), '[KRAKEN] received subscription event for unknown subscription', payload)
         }
 
-        return;
+        return
       }
 
-      this.emit('message', payload);
+      this.emit('message', payload)
     }
   }
 
-  subscribe(pair, subscription, options) {
-
-    if(this.pairs[pair] && this.pairs[pair].subscriptions.includes(subscription)) {
-      console.log(new Date, '[KRAKEN] refusing to subscribe to subscription twice', {pair, subscription});
-      return;
+  subscribe (pair, subscription, options) {
+    if (this.pairs[pair] && this.pairs[pair].subscriptions.includes(subscription)) {
+      console.log(new Date(), '[KRAKEN] refusing to subscribe to subscription twice', { pair, subscription })
+      return
     }
 
-    let hook;
-    let onReady = new Promise(r => hook = r);
+    let hook
+    const onReady = new Promise(resolve => { hook = resolve })
 
-    if(!this.pairs[pair]) {
+    if (!this.pairs[pair]) {
       this.pairs[pair] = {
         subscriptions: [],
         onReady: hook
-      };
+      }
     }
 
-    this.pairs[pair].subscriptions.push(subscription);
+    this.pairs[pair].subscriptions.push(subscription)
 
-    this._subscribe(pair, subscription, options);
+    this._subscribe(pair, subscription, options)
 
-    return onReady;
+    return onReady
   }
 
-  _subscribe(pair, subscription, options = {}) {
+  _subscribe (pair, subscription, options = {}) {
     this.ws.send(JSON.stringify(
       {
-        "event": "subscribe",
-        "pair": [ pair ],
-        "subscription": {
-          "name": subscription,
+        event: 'subscribe',
+        pair: [pair],
+        subscription: {
+          name: subscription,
           ...options
         }
       }
-    ));
+    ))
   }
-
 }
 
-export default Connection;
+export default Connection
